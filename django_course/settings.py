f7d1437f2c2ea39cca324b6782c24dceed838350
laurent <laurentbaltra@gmail.com>
2022-11-25 11:50:11 +0100

    add crispy-forms

--- django_course/settings.py
+++ django_course/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+     'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
